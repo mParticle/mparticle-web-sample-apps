f86d74918e21bf67aae4f1170f886905783e9e96
Alexander Sapountzis <alexs@mparticle.com>
2022-03-15 15:55:56 -0400

    Refactor Random ID Generator for simplicity

--- core-sdk-samples/higgs-shop-sample-app/src/contexts/UserDetails.tsx
+++ core-sdk-samples/higgs-shop-sample-app/src/contexts/UserDetails.tsx
@@ -19,27 +19,8 @@ interface UserDetailsStore {
 const UserDetails = createContext({} as UserDetailsStore);
 
 // Helper function to generate a fake UUID
-// UUID Generator inspired by: https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
-function generateUUID() {
-    let d = new Date().getTime();
-    let d2 =
-        (typeof performance !== 'undefined' &&
-            performance.now &&
-            performance.now() * 1000) ||
-        0; // Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        let r = Math.random() * 16; // random number between 0 and 16
-        if (d > 0) {
-            // Use timestamp until depleted
-            r = (d + r) % 16 || 0;
-            d = Math.floor(d / 16);
-        } else {
-            // Use microseconds since page-load if supported
-            r = (d2 + r) % 16 || 0;
-            d2 = Math.floor(d2 / 16);
-        }
-        return (c === 'x' ? r : (r && 0x3) || 0x8).toString(16);
-    });
+function generateID() {
+    return `${(Math.random() * 16) / 100}`.replace('.', '');
 }
 
 export function useUserDetails() {
@@ -66,7 +47,7 @@ const UserDetailsProvider: React.FC = ({ children }) => {
             const myUser: User = {
                 username,
                 email: `${username}@email.com`,
-                customerid: generateUUID(),
+                customerid: generateID(),
             };
 
             setUser(myUser);
