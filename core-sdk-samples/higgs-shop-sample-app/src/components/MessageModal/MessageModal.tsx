4698c8420a186c8a9dc2766ce5b52a8f0f46f720
Alexander Sapountzis <alexs@mparticle.com>
2022-07-12 11:32:35 -0400

    Change Env Config Modal button alignment

--- core-sdk-samples/higgs-shop-sample-app/src/components/MessageModal/MessageModal.tsx
+++ core-sdk-samples/higgs-shop-sample-app/src/components/MessageModal/MessageModal.tsx
@@ -2,6 +2,7 @@ import {
     Dialog,
     DialogActions,
     DialogContent,
+    Grid,
     Typography,
 } from '@mui/material';
 import React from 'react';
@@ -18,12 +19,30 @@ const MessageModal: React.FC<MessageModalProps> = ({
 }) => {
     return (
         <Dialog open={open}>
-            <DialogContent>
-                <Typography variant='body1' align='center'>
-                    {message}
-                </Typography>
-            </DialogContent>
-            {buttonAction && <DialogActions>{buttonAction}</DialogActions>}
+            <Grid
+                container
+                columns={1}
+                spacing={2}
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Grid item>
+                    <DialogContent>
+                        <Typography variant='body1' align='center'>
+                            {message}
+                        </Typography>
+                    </DialogContent>
+                </Grid>
+                {buttonAction && (
+                    <Grid item>
+                        <DialogActions>{buttonAction}</DialogActions>
+                    </Grid>
+                )}
+            </Grid>
         </Dialog>
     );
 };
