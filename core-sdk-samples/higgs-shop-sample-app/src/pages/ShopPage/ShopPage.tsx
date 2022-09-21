7e8006847c5c4e81d65146adec05635fa3f61666
Robert Ing <ring@mparticle.com>
2022-09-21 14:20:39 -0400

    Update sample app for demo-ing

--- core-sdk-samples/higgs-shop-sample-app/src/pages/ShopPage/ShopPage.tsx
+++ core-sdk-samples/higgs-shop-sample-app/src/pages/ShopPage/ShopPage.tsx
@@ -4,13 +4,27 @@ import { Box, Typography } from '@mui/material';
 import { Page } from '../../layouts/Page';
 import { ProductList } from '../../features/ProductDetails';
 import products from '../../models/Products';
+// import { SAMPLE_APP_GITHUB_REPOSITORY_URL } from '../../constants';
 
 const { mParticle } = window;
+
+declare global {
+    interface Window {
+        appboy: {
+            ab: {
+                Banner(arg0: string): void;
+            };
+            Banner(arg0: string): void;
+        };
+    }
+}
 
 const ShopPage = () => {
     // As per React conventions, it is recommended to trigger each mParticle
     // event in its own useEffect call.
     useEffect(() => {
+        // DEMO: uncomment the below to break the SDK when we are on appboy v3
+        // window.appboy.ab.Banner('test');
         // To simulate a pageview with an SPA, we are triggering a
         // PageView whenever the Shop Page component is mounted.
         // In the case of this example application, our Shop Page
