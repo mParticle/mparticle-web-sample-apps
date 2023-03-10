29eb33854d83506f01cf9547e653f64fb12c27cb
Robert Ing <rmi22186@gmail.com>
2023-03-10 15:08:11 -0500

    remove comments

--- core-sdk-samples/mp-sideloaded-kit-example/src/event-handler.js
+++ core-sdk-samples/mp-sideloaded-kit-example/src/event-handler.js
@@ -1,18 +1,3 @@
-/*
-A non-ecommerce event has the following schema:
-
-{
-    DeviceId: "a80eea1c-57f5-4f84-815e-06fe971b6ef2",
-    EventAttributes: {test: "Error", t: 'stack trace in string form'},
-    EventName: "Error",
-    MPID: "123123123123",
-    UserAttributes: {userAttr1: 'value1', userAttr2: 'value2'},
-    UserIdentities: [{Identity: 'email@gmail.com', Type: 7}]
-    User Identity Types can be found here:
-}
-
-*/
-
 function EventHandler(common) {
   this.common = common || {};
 }
--- core-sdk-samples/mp-sideloaded-kit-example/src/user-attribute-handler.js
+++ core-sdk-samples/mp-sideloaded-kit-example/src/user-attribute-handler.js
@@ -1,12 +1,3 @@
-/*
-The 'mParticleUser' is an object with methods on it to get user Identities and set/get user attributes
-Partners can determine what userIds are available to use in their SDK
-Call mParticleUser.getUserIdentities() to return an object of userIdentities --> { userIdentities: {customerid: '1234', email: 'email@gmail.com'} }
-For more identity types, see http://docs.mparticle.com/developers/sdk/javascript/identity#allowed-identity-types
-Call mParticleUser.getMPID() to get mParticle ID
-For any additional methods, see http://docs.mparticle.com/developers/sdk/javascript/apidocs/classes/mParticle.Identity.getCurrentUser().html
-*/
-
 function UserAttributeHandler(common) {
   this.common = common || {};
 }
