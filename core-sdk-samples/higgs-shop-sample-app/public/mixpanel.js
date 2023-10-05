b08e96a2c134c8c3c1c63d423e33a98af3b4d9f3
James <james@youknow.co.za>
2023-10-05 14:40:13 +0200

    remove mixpanel script

---
+++ core-sdk-samples/higgs-shop-sample-app/public/mixpanel.js
@@ -0,0 +1,17 @@
+//This function executes when a user views the home page
+function ViewHomePage() {
+
+}
+
+//This function executes when a user signs in with their login details
+function SignedIn() {
+
+}
+
+//This function executes when a user signs up with their sign up details
+function SignedUp() {
+
+}
+//This function executes when a user signs up with their sign up details
+
+}
