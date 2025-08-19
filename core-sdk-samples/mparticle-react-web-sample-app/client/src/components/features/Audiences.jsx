278f61337e5e41cdaf8277f8593001abbd1a301f
Jaissica Hora <jaissicahora@Jaissicas-MacBook-Pro.local>
2025-08-19 15:43:47 -0400

    syle: added clear log in audiences section for consistent UI

--- core-sdk-samples/mparticle-react-web-sample-app/client/src/components/features/Audiences.jsx
+++ core-sdk-samples/mparticle-react-web-sample-app/client/src/components/features/Audiences.jsx
@@ -10,7 +10,7 @@ const MOCK_AUDIENCE_BASE = 'http://localhost:4000/v1/'
 const Audiences = () => {
     const [logText, setLogText] = useState('')
     const { apiKey } = useConfig()
-
+    const clearLog = () => setLogText('')
     const log = useCallback((...args) => {
         setLogText(prev => prev + args.map(x => (typeof x === 'string' ? x : JSON.stringify(x, null, 2))).join(' ') + '\n')
     }, [])
@@ -35,6 +35,7 @@ const Audiences = () => {
             <div className="feature-row">
                 <div className="feature-input">
                     <button onClick={getAudiences}>Get Audiences</button>
+                    <button onClick={clearLog}>Clear Log</button>
                 </div>
                 <div className="feature-output">
                     <OutputLog logText={logText} />
